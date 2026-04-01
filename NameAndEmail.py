38133064871154d8a1b5e2f24f9c7f2be1bbd4f2
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-04-01 09:15:00 -0300

    Create NameAndEmail

---
+++ NameAndEmail.py
@@ -0,0 +1,7 @@
+# 1) Construa um programa que solicite ao usuário digitar o nome e o e-mail.
+# Exemplo de saída: “Meu nome é xxxxx e o meu e-mail é xxxx@xxxx.com.”
+
+nome = input('Digite seu nome: ')
+email = input('Digite seu email: ')
+
+print(f"Meu nome é {nome} e o meu e-mail é {email}")
