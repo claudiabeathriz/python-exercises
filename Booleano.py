7f97ba9c9aae3323006fbc7ce92863a14a9e0568
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-04-02 10:56:00 -0300

    Create Booleano

---
+++ Booleano.py
@@ -0,0 +1,18 @@
+# 6) Construa um programa que solicite a idade de um usuário e imprima na tela o valor
+# Booleano (True ou False) que responde à pergunta: "A idade informada é maior ou igual a
+# 18?".
+# ● Exemplo de execução 1:
+# ○ Usuário digita: 25
+# ○ Programa imprime: True
+# ● Exemplo de execução 2:
+# ○ Usuário digita: 15
+# ○ Programa imprime: False
+
+idade = int(input("Digite sua idade: "))
+
+print(idade >= 18)
+
+# if idade >= 18:
+#    print("True")
+# else:
+#    print("False")
