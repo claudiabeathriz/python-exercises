b18214654db2e974589410ae42c8810eb9d5951c
Claudia <claudiabopacheco@gmail.com>
2026-04-26 12:00:00 -0300

    fix: ArquivoPessoasFemMasc

--- ArquivoPessoasFemMasc.py
+++ ArquivoPessoasFemMasc.py
@@ -3,17 +3,13 @@ total_mulheres = 0
 total_homens = 0
 
 for pessoas in arquivo:
-    dados_pessoa = pessoas.split()
-    nome = dados_pessoa[0]
-    email = dados_pessoa[1]
-    sexo = dados_pessoa[2]
-    idade = dados_pessoa[3]
+    nome, email, sexo, idade = dados_pessoa = pessoas.split(',')
+    print(nome, email, sexo, idade, sep="\t")
 
     if sexo == 'F':
-        print(nome, email, sexo, idade, sep="\t")
         total_mulheres += 1
     else:
         total_homens += 1
-    print('Total de mulheres:', total_mulheres)
-    print('Total de homens:', total_homens)
-    arquivo.close()
+print('Total de mulheres:', total_mulheres)
+print('Total de homens:', total_homens)
+arquivo.close()
