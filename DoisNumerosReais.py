493b841190f23439aa894e0d9384a25949f6a4ee
Lucas Diniz <lucasdiniz.ps4@gmail.com>
2026-05-22 19:45:49 +0000

    Corrige cálculo da raiz quadrada e remove variável não utilizada

--- DoisNumerosReais.py
+++ DoisNumerosReais.py
@@ -6,6 +6,8 @@
 # d) o primeiro número elevado ao quadrado;
 # e) a raiz quadrada do segundo número.
 
+import math
+
 numero1 = float(input("Digite o primeiro numero: "))
 numero2 = float(input("Digite o segundo numero: "))
 
@@ -13,6 +15,6 @@ soma = numero1 + numero2
 produto = numero1 * numero2
 divisao = numero1 / numero2
 quadrado1 = numero1 ** 2
-quadrado2 = numero2 ** 2
+raiz = math.sqrt(numero2)
 
-print(f"Soma: {soma:.2f}, produto: {produto:.2f}, divisão: {divisao:.2f}, quadrado do primeiro número: {quadrado1:.2f}, quadrado do segundo número: {quadrado2:.2f}")
+print(f"Soma: {soma:.2f}, produto: {produto:.2f}, divisão: {divisao:.2f}, quadrado do primeiro número: {quadrado1:.2f}, quadrado do segundo número: {raiz:.2f}")
