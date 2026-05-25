13526f2949021e5a2e2be763cabfd490d0ef3f8c
Claudia <claudiabopacheco@gmail.com>
2026-05-25 12:00:00 -0300

    feat: add DuasListas

--- LAB/AulasP2/Aula08/DuasListas.py
+++ LAB/AulasP2/Aula08/DuasListas.py
@@ -2,12 +2,4 @@
 # inteiros. Uma das listas deve conter apenas os múltiplos de 3 presentes no intervalo [
 # 1, 1000 ], a outra deve conter o restante dos números neste intervalo, ou seja, os que
 # não são múltiplos de 3.
-# def f(v):
-# d = len(v)
-# q = ( v[0] + v[d-1] ) / 2
-# return q
-# n = int( input("Digite n:") )
-# x = []
-# for i in range(n//10,n):
-# x.append(i*10)
-# y = f(x)
+
