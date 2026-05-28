6c1dc4d0ae108bb379276956b1bb952819bc8ace
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-05-28 10:56:00 -0300

    feat: add RemoverValor

--- PROG1/listaFunções/ObterValorExcluido.py
+++ PROG1/listaFunções/ObterValorExcluido.py
@@ -2,7 +2,7 @@
 # retorne posição onde ele foi encontrado ou -1 caso não esteja na lista, mas
 # utilizando uma função
 #
-def proc_valor_remov(lista,valor):
+def retornar_pos(lista,valor):
     for i in range(len(lista)):
         if lista[i] == valor:
             return i
@@ -11,4 +11,4 @@ def proc_valor_remov(lista,valor):
 lista = [1, 2, 10, 5, 20]
 valor = 10
 
-print(proc_valor_remov(lista,valor))
+print(retornar_pos(lista,valor))
