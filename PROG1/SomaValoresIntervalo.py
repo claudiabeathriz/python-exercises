cd42ce6f79265d4d528fe0b3c36f44b043564d22
Claudia <claudiabopacheco@gmail.com>
2026-06-08 16:45:51 -0300

    commit

---
+++ PROG1/SomaValoresIntervalo.py
@@ -0,0 +1,7 @@
+# Escreva um programa que leia dois valores inteiros que formarão um intervalo,
+# posteriormente, leia um arquivo contendo números inteiros e calcula a soma dos
+# números pares compreendidos entre dois números lidos.
+
+
+
+arquivo = open('valoresintervalo.dat', 'r')
